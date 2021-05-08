044552c5d6fda1eaba68e7b862b926a7ee7986c6
kyorohiro <kyorohiro@gmail.com>
2021-05-09 02:41:11 +0900

    use tailwindcss

--- proj/my-app/src/index.tsx
+++ proj/my-app/src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="chat-notification">
+      <div className="chat-notification-logo-wrapper">
+        <img className="chat-notification-logo" src="/img/logo.svg" alt="ChitChat Logo" />
+      </div>
+      <div className="chat-notification-content">
+        <h4 className="chat-notification-title">ChitChat</h4>
+        <p className="chat-notification-message">You have a new message!</p>
+      </div>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
